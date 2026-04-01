688c31e563e6391259532d345e91582abd99f92e
Atti3284 <Nagyok3248994@gmail.com>
2026-04-01 20:44:09 +0200

    probalgatas

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Szakdolgozat Project
         </a>
       </header>
     </div>
